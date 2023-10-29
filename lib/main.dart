04286eaa63845cd842a50c4bcf3bc2b9a775c5bd
rizasyamaidzar <71583800+rizasyamaidzar@users.noreply.github.com>
2023-10-29 20:49:22 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:praktikum4/widgets/inputSuhu.dart';
+import 'package:praktikum4/widgets/konversiSuhu.dart';
+import 'package:praktikum4/widgets/riwayat.dart';
+import 'package:praktikum4/widgets/targetPerhitungan.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +18,6 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   TextEditingController suhu = new TextEditingController();
   //variabel berubah
-  String jenisSuhu = '';
   double inputUser = 0;
   String selectedValue = 'Kelvin';
   List<String> dropdownItems = ['Kelvin', 'Reamur','Fahrenheit'];
@@ -24,15 +27,20 @@ class _MyAppState extends State<MyApp> {
     return setState(() {
       if(suhu.text.isNotEmpty){
       inputUser = double.parse(suhu.text);
-      if (jenisSuhu == "Kelvin") {
+      if (selectedValue == "Kelvin") {
         inputUser += 273.15;
-      } else if(jenisSuhu=="Reamur") {
-        inputUser = (4 / 5) * inputUser;
+      } else if(selectedValue=="Reamur") {
+        inputUser = inputUser * (4 / 5) ;
       }else{
        inputUser = (inputUser * 9/5)+32;
       }
-      listViewItem.add("$jenisSuhu : " + "$inputUser");
+      listViewItem.add("$selectedValue : " + "$inputUser");
     }
+    });
+  }
+  void onDropdownChanged(Object? value){
+    return setState(() {
+      selectedValue = value.toString();
     });
   }
 
@@ -55,63 +63,31 @@ class _MyAppState extends State<MyApp> {
           ),
           body: Column(
             children: [
-              TextFormField(
-                textAlign: TextAlign.center,
-                controller: suhu,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  hintText: "Input celcius"
-                ),
-              ),
+              InputSuhu(suhu: suhu),
               SizedBox(height: 10),
-              DropdownButton(
-                  value: selectedValue,
-                  items: dropdownItems.map((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                  onChanged: (value) {
-                    setState(() {
-                      selectedValue = "$value";
-                      jenisSuhu = "$value";
-                    });
-                  }),
+              TargetPerhitungan(
+                selectedValue: selectedValue, 
+                dropdownItems: dropdownItems, onDropdownChanged: onDropdownChanged,
+                ),
               Text("Hasil",style: TextStyle(
                 fontSize: 40,
               ),),
               Text("$inputUser",style: TextStyle(fontSize: 50),),
-              Container(
-                margin: EdgeInsets.only(top: 50),
-                width: 1000,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Color.fromARGB(255, 0, 110, 253), // background
-                    onPrimary: Colors.white, // foreground
-                  ),
-                  onPressed: () {
-                    konversiSuhu();
-                  },
-                  child: Text('Konversi suhu'),
-                ),
-              ),
+              KonversiSuhu(onPressed: konversiSuhu,),
               SizedBox(height: 10,),
               Text("Konversi Suhu",
                 style: TextStyle(fontSize:25)),
-              Expanded(
-                  child: ListView.builder(
-                      itemCount: listViewItem.length,
-                      itemBuilder: (context, index) {
-                        return Container(
-                          margin: EdgeInsets.all(10),
-                          child: Text(listViewItem[index],style: TextStyle(
-                            fontSize: 20
-                          ),)
-                          );
-                      }))
+              RiwayatPerhitungan(listViewItem: listViewItem)
             ],
           ),
         ));
   }
 }
+
+
+
+
+
+
+
+
